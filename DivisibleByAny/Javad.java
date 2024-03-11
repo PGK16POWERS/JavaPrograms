be65e0f8e0b08f1c7cb8d7298170db92091abf4a
Liberia (Kaiberius) Kartier <136324051+PGK16POWERS@users.noreply.github.com>
2024-03-11 16:46:24 +0000

    done

---
+++ DivisibleByAny/Javad.java
@@ -0,0 +1,29 @@
+// IMPORT SCANNER CLASS
+import java.util.Scanner;
+
+public class Javad {
+    public static void main(String[] args) {
+        // CREATE AN INSTANCE OF THE SCANNER CLASS
+        Scanner s = new Scanner(System.in);
+
+        // CREATE INT VARIABLES 
+        int dividend, divisor;
+
+        // PROMPT USER TO INITIALIZE EACH VARIABLE RESPECTIVELY
+        System.out.println("Enter divisor");
+        divisor = s.nextInt();
+
+        System.out.println("Enter dividend");
+        dividend = s.nextInt();
+
+        // CREATE IF-ELSE STATEMENT FOR EVALUATIONS BETWEEN DIVIDEND AND DIVISOR
+        if ( dividend % divisor == 0) {
+            System.out.println(dividend + " is divisible by " + divisor);
+            System.out.println(divisor + " goes into " + dividend + ": " + dividend / divisor + " times");
+        } else {
+            System.out.println(dividend + " is not divisble by " divisor);
+        }
+
+        s.close();
+    }
+}
