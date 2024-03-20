d3e1c2521ec09a96844fbd62b49f341e3cf79c24
Liberia (Kaiberius) Kartier <136324051+PGK16POWERS@users.noreply.github.com>
2024-03-20 19:59:32 +0000

    jFileComplete

--- SumOfDigits/Javas1.java
+++ SumOfDigits/Javas1.java
@@ -1,1 +1,30 @@
-Java
+// IMPORT SCANNER CLASS
+import java.util.Scanner;
+
+public class Javas1 {
+    public static void main(String[] args) {
+        // CREATE INSTANCE OF SCANNER CLASS
+        Scanner s = new Scanner(System.in);
+
+        // PROMPT USER TO INPUT NUMBER
+        System.out.print("Enter a number: ");
+        int num = s.nextInt();
+
+        int sum = 0;
+
+        /*
+            CREATE WHILE CONDITIONAL STATEMENT
+            CALCULATE THE SUM OF THE NUMBER INPUTED BY THE USER
+        */ 
+        while (num != 0) {
+            sum += num % 10;
+
+            num /= 10;
+        }
+
+        // OUTPUT FINDINGS
+        System.out.println("The sum of the given number is: " + sum);
+
+        s.close();
+    }
+}
