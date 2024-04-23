b9ba9fe2bdb76dad88360056b2acb6b51cf8cd11
programmer Kartier <progrmmerKartier@gmail.com>
2024-04-23 09:00:43 +0200

    new Edition

---
+++ JavaArrayElementCounter/JavaFile.java
@@ -0,0 +1,11 @@
+package JavaArrayElementCounter;
+
+public class JavaFile {
+    public static void main(String[] args) {
+        // CREATE AND INITIALIZE YOUR ARRAY
+        int[] array = {2,4,5,6,7,8,9,0};
+
+        // OUTPUT THE LENGTH OF THE ARRAY USING THE BUILT-IN .LENGTH METHOD
+        System.out.println("The length of the array is: " + array.length);
+    }
+}
---
+++ JavaArrayElementCounter/README.md
@@ -0,0 +1,6 @@
+# Java Array Element Counter
+Simple Java program that uses the built-in **.length** method to efficiently find out how many elements can be found in an array.
+
+## Steps for implementation:
+1. (Inside the main method) Create and initialize the array.
+2. Output in the terminal the length of the array.
